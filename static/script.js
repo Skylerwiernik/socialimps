a46f89c58b7da88a7a44a09af75d26868d851d07
Skyler Wiernik <skylerwiernik@gmail.com>
2023-04-11 00:43:15 -0400

    Initial commit

---
+++ static/script.js
@@ -0,0 +1,12 @@
+  var sections = document.getElementsByClassName("section");
+  for (var i = 0; i < sections.length; i++) {
+    sections[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
